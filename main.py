29842b4b187d0004fc5848d8bbd76f8b719a4fa5
Clément Porhiel <clement.porhiel@gmail.com>
2022-01-10 15:36:15 +0100

    formating refactor

--- main.py
+++ main.py
@@ -1,168 +1,133 @@
-import cv2
-import numpy as np
+import os
 import math
+import cv2 as cv
+import numpy as np
+from PIL import Image
 
-# Importing HARR CASCADE XML file
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+PATH          = os.getcwd()
+DATASET_NAME  = "dataset"
+DATASET_PATH  = PATH + "/" + DATASET_NAME
+ANALYSED_NAME = "modified"
+ANALYSED_PATH = DATASET_PATH + "/" + ANALYSED_NAME
 
-base = 'test'
-img_name = 'benjamin'
-# Uploading test image
-img = base + '/' + img_name + '.jpg'
-img = cv2.imread(img)
+DEST_WIDTH = 178
+DEST_HEIGHT = 218
+REF_ED = 39
+REF_ELX = 69
+REF_EY = 110
 
-avg_color_per_row = np.average(img, axis=0)
-avg_color = np.average(avg_color_per_row, axis=0)
+FACE_CASCADE = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+EYE_CASCADE  = cv.CascadeClassifier('haarcascade_eye.xml')
 
-# Displaying the image
-cv2.imshow('Detected Face Image', img)
+def distance(p1, p2):
+    return math.sqrt( (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
-# Waiting for escape key for image to close
-cv2.waitKey()
+def init_result_folder():
+    if not os.path.exists(ANALYSED_PATH):
+        os.makedirs(ANALYSED_PATH)
+    
 
-height, width, channels = img.shape
-center = (width / 2, height / 2)
-print("Largeur:", height, ", Longueur:", width, "De l'image")
+def get_images_list():
+    return [f for f in os.listdir(DATASET_PATH) if os.path.isfile(os.path.join(DATASET_PATH, f))]
 
-# Valeur des images de celebA
-eyesGap = 39
-eyesLargeur = 89.0
-eyesLongueur = 109.0
-dimensions = (178, 218)
+def face_detection(image_name):
+    path = DATASET_PATH + "/" + image_name
+    img = cv.imread(path)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    faces = FACE_CASCADE.detectMultiScale(gray, 1.1, 6)
+    for face in faces:
+        (fx, fy, fw, fh) = face
+        roi_color = img[fy:fy + fh, fx:fx + fw]
+        roi_gray = gray[fy:fy + fh, fx:fx + fw]
+        le, re = eyes_detection(roi_gray, face)
+        if le is not None and re is not None:
+            imgage = Image.open(path)
+            imgage = normalize_image(image_name, le, re)
+            imgage.save(ANALYSED_PATH + "/" + image_name)
 
-# Converting to grey scale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def eyes_detection(image, face):
+    (fx, fy, fw, fh) = face
+    lec = rec = None
+    eyes = EYE_CASCADE.detectMultiScale(image)
+    if len(eyes) == 2:
+        if eyes[0][0] < eyes[1][0]:
+            (lex, ley, lew, leh) = le = eyes[0]
+            (rex, rey, rew, reh) = re = eyes[1]
+        else:
+            (lex, ley, lew, leh) = le = eyes[1]
+            (rex, rey, rew, reh) = re = eyes[0]
+        lec = int((lex + (lew / 2)) + fx), int((ley + (leh / 2)) + fy)
+        rec = int((rex + (rew / 2)) + fx), int((rey + (reh / 2)) + fy)
+    return lec, rec
+
+def rotate_image(image, el, er, resample=Image.BICUBIC):
+    center = el
+    direction = er[0] - el[0], er[1] - el[1]
+    angle = -math.atan2(float(direction[1]), float(direction[0]))
+    if center is None:
+        return image.rotate(angle=angle, resample=resample)
+    center_x, center_y = center
+    scale_x = scale_y = 1.0
+    cosinus = math.cos(angle)
+    sinus = math.sin(angle)
+    a = cosinus / scale_x
+    b = sinus / scale_x
+    c = center_x - (center_x * a) - (center_y * b)
+    d = - sinus / scale_y
+    e = cosinus / scale_y
+    f = center_y - (center_x * d) - (center_y * e)
+    return image.transform(image.size, Image.AFFINE, (a, b, c, d, e, f), resample=resample)
+
+def scale_image(image):
+    return image.resize((DEST_WIDTH, DEST_HEIGHT), Image.ANTIALIAS)
+
+def crop_image(image, el, er, avg_color):
+    ed = distance (el, er)
+    scale = float(ed) / float(REF_ED)
+    crop_ul = el[0] - int(scale * REF_ELX), el[1] - int(scale * REF_EY)
+    crop_size = int(DEST_WIDTH * scale), int(DEST_HEIGHT * scale)
+    crop_dr = crop_ul[0] + crop_size[0], crop_ul[1] + crop_size[1]
+
+    return image.crop((crop_ul[0], crop_ul[1], crop_dr[0], crop_dr[1]))
+
+    if crop_ul[0] < 0:
+        image = cv.hconcat([blank_image, image])
+        blank_image = cv.hconcat([blank_image, blank_image])
+        print("coucou 1")
+        crop_ul[0] += width
+        crop_dr[0] += width
+    if crop_dr[0] > width:
+        image = cv.hconcat([image, blank_image])
+        blank_image = cv.hconcat([blank_image, blank_image])
+        print("coucou 2")
+    if crop_ul[1] < 0:
+        image = cv.vconcat([blank_image, image])
+        blank_image = cv.vconcat([blank_image, blank_image])
+        print("coucou 3")
+        crop_ul[1] += height
+        crop_dr[1] += height
+    if crop_dr[1] > height:
+        image = cv.vconcat([image, blank_image])
+        blank_image = cv.vconcat([blank_image, blank_image])
+        print("coucou 4")
+
+    return image[
+        crop_ul[1]:crop_dr[1],
+        crop_ul[0]:crop_dr[0]
+    ]
+
+def normalize_image(image_name, el, er):
+    image = Image.open(DATASET_PATH + "/" + image_name)
+    avg_color_per_channel = np.average(image, axis=0)
+    avg_color = np.average(avg_color_per_channel, axis=0)
+    
+    image = rotate_image(image, el, er)
+    image = crop_image(image, el, er, avg_color)
+    image = scale_image(image)
+    return image
 
 
-def distance(xa, ya, xb, yb):
-    return math.sqrt((xa - xb) ** 2 + (ya - yb) ** 2)
-
-
-def rotate_image(image2, angle):
-    rotate_matrix = cv2.getRotationMatrix2D(center=center, angle=angle, scale=1)
-    rotated_image = cv2.warpAffine(src=image2, M=rotate_matrix, dsize=(width, height), borderValue=avg_color)
-    cv2.waitKey(0)
-
-    return rotated_image
-
-
-def crop(image, fCenter, rapport):
-    # x,y du point en haut à gauche de la nouvelle image
-    xa = int(fCenter[0] - (eyesLargeur * rapport))
-    ya = int(fCenter[1] - (eyesLongueur * rapport))
-
-    # x,y du point en bas à droite de la nouvelle image
-    xb = int(fCenter[0] + (eyesLargeur * rapport))
-    yb = int(fCenter[1] + (eyesLongueur * rapport))
-    crop_img = image
-
-    blank_image = cv2.imread('test/' + img_name + '.jpg')
-    blank_image[:] = avg_color
-
-    if xa < 0:
-        crop_img = cv2.hconcat([blank_image, crop_img])
-        blank_image = cv2.hconcat([blank_image, blank_image])
-        xa = width + xa
-        xb = width + xb
-
-    if ya < 0:
-        crop_img = cv2.vconcat([blank_image, crop_img])
-        blank_image = cv2.vconcat([blank_image, blank_image])
-        ya = height + ya
-        yb = height + yb
-
-    if xb > width:
-        crop_img = cv2.hconcat([crop_img, blank_image])
-        blank_image = cv2.hconcat([blank_image, blank_image])
-
-    if yb > height:
-        crop_img = cv2.vconcat([crop_img, crop_img])
-        blank_image = cv2.vconcat([blank_image, blank_image])
-
-    # On rogne l'image
-    print(xa, ya, xb, yb)
-    crop_img = crop_img[ya:yb, xa:xb]
-    return crop_img
-
-
-def resize(image2):
-    resize_image = cv2.resize(image2, dimensions, interpolation=cv2.INTER_LINEAR)
-    return resize_image
-
-
-# Allowing multiple scale(Multiple size) detection
-faces = face_cascade.detectMultiScale(gray, 1.1, 6)
-
-# Creating Rectangle around face
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 250), 2)
-    roi_gray = gray[y:y + h, x:x + w]
-    roi_color = img[y:y + h, x:x + w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex, ey, ew, eh) in eyes:
-        cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-
-    img_vierge = 'test/' + img_name + '.jpg'
-    img_vierge = cv2.imread(img_vierge)
-
-    if len(eyes) > 0:
-        xle = int(eyes[0][0]) + (int(eyes[0][3]) // 2) + x
-        yle = int(eyes[0][1]) + (int(eyes[0][3]) // 2) + y
-        xre = int(eyes[1][0]) + (int(eyes[1][3]) // 2) + x
-        yre = int(eyes[1][1]) + (int(eyes[1][3]) // 2) + y
-        print(xle, yle, xre, yre)
-
-        # Calcul des deux distances
-        ac = distance(xre, yle, xle, yle)
-        ab = distance(xre, yle, xre, yre)
-
-        # Face center
-        fCenter = (min(xle, xre) + (ac / 2), yle)
-
-        # Calcul de l'angle de rotation
-        c = math.degrees(math.atan(ab / ac))
-
-        # Calcul de proportionalités
-        rapport = ac / eyesGap
-
-        inv = 1
-        if xle > xre:
-            inv = -inv
-        if yle > yre:
-            inv = -inv
-
-        # Rotation de l'image
-        img_vierge = rotate_image(img_vierge, inv * c)
-        img = rotate_image(img, inv * c)
-        cv2.imshow('Rotated Image', img)
-        cv2.waitKey()
-
-        # On rogne l'image
-        img_vierge = crop(img_vierge, fCenter, rapport)
-        img = crop(img, fCenter, rapport)
-
-        cv2.imshow('Croped Image', img)
-        cv2.waitKey()
-
-        # On redimensionne l'image en 178*218
-        img_vierge = resize(img_vierge)
-        img = resize(img)
-        # Displaying the image
-        cv2.imshow('Detected Face Image', img_vierge)
-        # Waiting for escape key for image to close
-        cv2.waitKey()
-    else:
-        print('AUCUN OEIL DETECTE')
-
-    # Creating Rectangle around face
-    for (ex, ey, ew, eh) in eyes:
-        cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-
-    cv2.imwrite(base + '/Modified/' + img_name + '.jpg', img_vierge)
-
-# Displaying the image
-# cv2.imshow('Detected Face Image', img)
-
-# Waiting for escape key for image to close
-# cv2.waitKey()
+if __name__ == "__main__":
+    init_result_folder()
+    for img_name in get_images_list():
+        face_detection(img_name)
